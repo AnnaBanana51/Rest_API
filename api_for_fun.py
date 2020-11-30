ec5f810935948fb59d0bfe2badada50f18dad0fb
Anna Levy <65465147+AnnaBanana51@users.noreply.github.com>
2020-11-29 22:44:58 -0500

    Add files via upload

---
+++ api_for_fun.py
@@ -0,0 +1,79 @@
+
+from flask import Flask, request, jsonify
+from flask_restful import Resource, Api, reqparse
+
+app = Flask(__name__)
+api = Api(app)
+
+STUDENTS = {
+    '1':{'name': 'Anna', 'grade': '100', 'spec': 'Data Science'},
+    '2':{'name': 'Bryant', 'grade': '100', 'spec': 'Data Science'},
+    '3':{'name': 'Clariza', 'grade': '100', 'spec': 'Data Science'},
+    '4':{'name': 'Claudia', 'grade': '100', 'spec': 'Data Science'},
+    '5':{'name': 'Elston', 'grade': '100', 'spec': 'Data Science'},
+    '6':{'name': 'Fausto', 'grade': '100', 'spec': 'Data Science'},
+    '7':{'name': 'Gyasi', 'grade': '100', 'spec': 'Data Science'},
+    '8':{'name': 'Irene', 'grade': '100', 'spec': 'Data Science'},
+    '9':{'name': 'Ishraq', 'grade': '100', 'spec': 'Data Science'},
+    '10':{'name': 'Janet', 'grade': '100', 'spec': 'Data Science'},
+    '11':{'name': 'Juan', 'grade': '100', 'spec': 'Data Science'},
+    '12':{'name': 'Judi', 'grade': '100', 'spec': 'Data Science'},
+    '13':{'name': 'Kari', 'grade': '100', 'spec': 'Data Science'},
+    '14':{'name': 'Luis', 'grade': '100', 'spec': 'Data Science'},
+}
+
+parser = reqparse.RequestParser()
+
+class StudentList(Resource):
+    def get(self):
+        return STUDENTS
+    
+    def post(self):
+        parser.add_argument("name")
+        parser.add_argument("grade")
+        parser.add_argument("spec")
+        args = parser.parse_args()
+        student_id = int(max(STUDENTS.keys())) + 1
+        student_id = '%i' % student_id
+        STUDENTS[student_id] = {
+            "name": args["name"],
+            "grade": args["grade"],
+            "spec": args["spec"],
+        }
+        return STUDENTS[student_id], 201
+        # return STUDENTS
+
+class Student(Resource):
+    def get(self, student_id):
+        if student_id not in STUDENTS:
+            return 'Not found', 404
+        else:
+            return STUDENTS[student_id]   
+    def put(self, student_id):
+        parser.add_argument("name")
+        parser.add_argument("grade")
+        parser.add_argument("spec")
+        args = parser.parse_args()
+        if student_id not in STUDENTS:
+            return 'Record not found', 404
+        else:
+            student = STUDENTS[student_id]
+            student["name"] = args["name"] if args["name"] is not None else student["name"]
+            tudent["grade"] = args["grade"] if args["grade"] is not None else student["grade"]
+            tudent["spec"] = args["spec"] if args["spec"] is not None else student["spec"]
+            return student, 200
+    def delete(self, student_id):
+        if student_id not in STUDENTS:
+            return 'Not found', 404
+        else:
+            del STUDENTS[student_id]
+            return '', 204
+
+
+api.add_resource(StudentList, '/students/')    
+api.add_resource(Student, '/students/<student_id>')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
